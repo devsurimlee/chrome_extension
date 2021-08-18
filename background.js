2f3a5a0ad00023bc007f6edb0cb49c5842d4e193
srlee <devsurimlee@gmail.com>
2021-08-18 15:57:20 +0900

    click event 적용중

--- background.js
+++ background.js
@@ -2,6 +2,6 @@ let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener( () => {
     chrome.storage.sync.set({ color });
-    console.log('background color is ' + color);
+    console.log('background:: color is ' + color);
 
 });
--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,17 @@
 // Initialize button with user's preferred color
+
+console.log('test2');
+
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
     changeColor.style.backgroundColor = color;
 });
+
+// click event
+changeColor.addEventListener('click', async () => {
+   console.log('test!');
+});
+
+
 
