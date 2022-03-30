0eae5b1c767c4173b44e43c824c3dd1009a6841c
srlee <devsurimlee@gmail.com>
2022-03-30 13:17:56 +0900

    feat: 아이콘 클릭시 아이콘 on/off 이벤트 추가

--- background.js
+++ background.js
@@ -1,10 +1,37 @@
-//기본색 (초록)
-let color = '#3aa757';
-// 배경 리셋용 색상(흰색)
-let defaultColor = '#ffffff';
 
-//색상 코드 세팅
-chrome.runtime.onInstalled.addListener( () => {
-    chrome.storage.sync.set({ color });
-    chrome.storage.sync.set({ defaultColor });
+let isSwitchON = true
+const onIconImg = "/images/on.png";
+const offIconImg = "/images/off.png";
+
+
+
+//아이콘 클릭 이벤트
+chrome.action.onClicked.addListener((tab) => {
+	if(isSwitchON) {
+		isSwitchON = false
+		chrome.action.setIcon({path: offIconImg});
+
+	} else {
+		isSwitchON = true
+		chrome.action.setIcon({path: onIconImg});
+	}
 });
+
+
+
+
+
+
+////////////////////////////////////////////////
+// 예제
+//
+// //기본색 (초록)
+// let color = '#3aa757';
+// // 배경 리셋용 색상(흰색)
+// let defaultColor = '#ffffff';
+//
+// //색상 코드 세팅
+// chrome.runtime.onInstalled.addListener( () => {
+//     chrome.storage.sync.set({ color });
+//     chrome.storage.sync.set({ defaultColor });
+// });
