7a521ea7f9e162e2c44955d0bdc5d4689b284535
srlee <devsurimlee@gmail.com>
2022-04-05 16:22:06 +0900

    feat: 아이콘 클릭시 설정용 팝업 추가

--- js/popup.js
+++ js/popup.js
@@ -1,47 +1,14 @@
-// 배경색 변경 버튼
-let changeColor = document.getElementById("changeColor");
-// 배경색 리셋하는 버튼
-let resetColor = document.getElementById("resetColor");
+let onImageBlock = document.getElementById("onImageBlock");
+let offImageBlock = document.getElementById("offImageBlock");
 
-chrome.storage.sync.get("color", ({ color }) => {
-    changeColor.style.backgroundColor = color;
-});
+let onRefresh = document.getElementById("onRefresh");
+let offRefresh = document.getElementById("offRefresh");
+
 
 document.addEventListener("click", async (e) => {
-    await btnClickEvent(e.target);
+	await btnClickEvent(e.target);
 });
 
 async function btnClickEvent(object) {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    if(object == changeColor) {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: setPageBackgroundColor,
-        });
-    }
-
-    if(object == resetColor) {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: resetPageBackgroundColor,
-        });
-    }
 }
-
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
-}
-
-function resetPageBackgroundColor() {
-    chrome.storage.sync.get("defaultColor", ({ defaultColor }) => {
-        document.body.style.backgroundColor = defaultColor;
-    });
-}
-
-// setPageBackgroundColor <-- 전체 배경화면 색 변경
-// setPageBackgroundColor() <-- popup.html의 배경화면 색이 변경됨(default_popup)
-// 무슨 차이인지 찾아보기
-
