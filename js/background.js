e0399945d2faa86fdbf981bc84f308021622193f
srlee <devsurimlee@gmail.com>
2022-04-06 16:45:00 +0900

    feat: 팝업 > 이미지블락 라디오 클릭시 on/off 구현

---
+++ js/background.js
@@ -0,0 +1,62 @@
+const onIconImg = "/images/on-48.png";
+const offIconImg = "/images/off-48.png";
+
+
+chrome.storage.sync.get('isSwitchON', (e) => {
+	if (e.isSwitchON) {
+		changeIcon(onIconImg);
+	} else {
+		changeIcon(offIconImg);
+	}
+});
+
+function changeIcon(img) {
+	chrome.action.setIcon({path: img});
+}
+
+
+// let isSwitchON = false
+// const onIconImg = "/images/on-48.png";
+// const offIconImg = "/images/off-48.png";
+
+
+//아이콘 클릭 이벤트
+// chrome.action.onClicked.addListener((tab) => {
+// 	if (isSwitchON) {
+// 		isSwitchON = false
+// 		changeIcon(offIconImg);
+// 	} else {
+// 		isSwitchON = true
+// 		changeIcon(onIconImg);
+// 	}
+// 	handleImage();
+// });
+//
+//
+// function changeIcon(img) {
+// 	chrome.action.setIcon({path: img});
+// }
+//
+// function handleImage() {
+// 	if (chrome.contentSettings) {
+// 		chrome.contentSettings["images"].set({
+// 			primaryPattern: "<all_urls>",
+// 			setting: isSwitchON ? 'block' : 'allow'
+// 		});
+// 	}
+// }
+
+
+////////////////////////////////////////////////
+// 예제
+//
+// //기본색 (초록)
+// let color = '#3aa757';
+// // 배경 리셋용 색상(흰색)
+// let defaultColor = '#ffffff';
+//
+// //색상 코드 세팅
+// chrome.runtime.onInstalled.addListener( () => {
+//     chrome.storage.sync.set({ color });
+//     chrome.storage.sync.set({ defaultColor });
+// });
