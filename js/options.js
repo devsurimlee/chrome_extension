f04516d9bb108f8ce3682579976ce44d1242a108
srlee <devsurimlee@gmail.com>
2022-04-15 12:35:35 +0900

    feat: 옵션 > 화이트리스트 등록시 저장하고, 옵션 재진입시 도메인 로드함

--- js/options.js
+++ js/options.js
@@ -2,8 +2,9 @@ const resetWhiteListBtn = document.getElementById("resetWhiteListBtn");
 const saveWhiteListBtn = document.getElementById("saveWhiteListBtn");
 const whiteList = document.getElementById("whiteList");
 
-resetWhiteListBtn.addEventListener("click", resetWhiteList);
 saveWhiteListBtn.addEventListener("click", handleWhiteList);
+resetWhiteListBtn.addEventListener("click", resetWhiteList);
+loadWhiteList();
 
 
 function resetWhiteList() {
@@ -12,11 +13,21 @@ function resetWhiteList() {
 
 function handleWhiteList() {
 	parsingDomain();
-	//save domain
+	saveWhiteList();
 }
 
 function parsingDomain() {
 	const domain_pattern = /(?!w{1,}\.)(\w+\.?)([a-zA-Z]+)(\.\w+)/g;
 	whiteList.value = whiteList.value.match(domain_pattern);
 	whiteList.value = whiteList.value.replaceAll(',', ', ');
+}
+
+function saveWhiteList() {
+	chrome.storage.sync.set({"whiteList": whiteList.value});
+}
+
+function loadWhiteList() {
+	chrome.storage.sync.get("whiteList", (e) => {
+		whiteList.value = e.whiteList;
+	});
 }
